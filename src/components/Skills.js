8b7754462b18e040bf1012f66845f31ed19346ca
Steven David Pillay <98861688+code-Gambler@users.noreply.github.com>
2024-11-16 17:03:09 -0500

    Completed the skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { skills } from '../data';
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <section id='skills' className='section bg-[#f8f3eca1] pb-32'>
+        <div className="container mx-auto text-center">
+            <h2 className='text-5xl font-primary font-extrabold mb-4'>
+                What we do
+                </h2>
+                <p className='max-w-[540px] mx-auto px-6 lg:px-0 mb-[60px]'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel, eligendi reiciendis corporis quasi deserunt cum fugiat corrupti quam cupiditate quaerat!
+                </p>
+                {/* Skills grid */}
+                <div className='lg:grid lg:grid-cols-3 gap-12'>
+                    {skills.map((skill, index) => {
+                        const { icon, title, description } = skill;
+                        return <div key={index} className='flex flex-col items-center justify-center mb-16 last:mb-0 lg:mb-0'>
+                            <img src={icon} alt="" className='mb-6' />
+                            <h4 className='text-2xl mb-2 font-primary font-bold'>{title}</h4>
+                            <p className='max-w-[332px] lg:max-w-[350px]'>{description}</p>
+                        </div>
+                    })}
+                </div>
+        </div>
+    </section>
   )
 }
 
