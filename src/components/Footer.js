735f5f124f0b4baa602bcee6aa13e8634cc4b5fa
Steven David Pillay <98861688+code-Gambler@users.noreply.github.com>
2024-11-16 17:59:57 -0500

    Added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-primary-darker py-[24px] text-white text-sm text-center'>2024 &copy; Steven David Pillay</footer>
   )
 }
 
