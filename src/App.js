139dc344a22dbb66c4e44b52f2d2a54bef8ab70d
Steven David Pillay <98861688+code-Gambler@users.noreply.github.com>
2024-11-16 17:54:09 -0500

    Completed the socials and the contact section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Testimonials from './components/Testimonials';
 import Skills from './components/Skills';
 import Team from './components/Team';
 import Newsletter from './components/Newsletter';
+import Contact from './components/Contact';
 
 const App = () => {
   return <div>
@@ -14,6 +15,7 @@ const App = () => {
     <Skills/>
     <Team />
     <Newsletter />
+    <Contact />
     <div className='h-[2000px]'></div>
   </div>;
 };
---
+++ src/components/Contact.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { social } from '../data';
+
+
+const Contact = () => {
+  return (
+    <section id='contact' className='bg-primary text-white min-h-[732px] section'>
+        <div className="container mx-auto text-center">
+            <h2 className='text-5xl font-primary font-extrabold mb-4'>Contact us</h2>
+            <p className='max-w-[540px] mx-auto px-6 lg:px-0 mb-[64px]'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellat voluptas beatae accusantium non, laborum aperiam corporis ratione iste vel.
+            </p>
+            {/* form */}
+            <form className='px-8 lg:px-0 max-w-[600px] mx-auto flex flex-col space-y-6 mb-[46px]'>
+                <input className='form-control' placeholder='Your name' type='text'/>
+                <input className='form-control' placeholder='Your email address' type='email'/>
+                <textarea className='textarea' placeholder='Your message'></textarea>
+                <button className='btn bg-accent hover:bg-accent-hover transition-all'>Send message</button>
+            </form>
+            {/* socials */}
+            <div className='flex items-center justify-between max-w-[250px] mx-auto'>
+                {social.map((item, index) => {
+                    const { icon, link } = item;
+                    return <a key={index} href={link}>
+                        <img src={icon} alt="" /></a>
+                })}
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Contact
