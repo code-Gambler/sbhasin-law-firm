2e3459b659c5bcf0c51ba2a9897a4f499d6a444d
Steven David Pillay <98861688+code-Gambler@users.noreply.github.com>
2024-11-16 17:05:19 -0500

    Added boilerplate code for team section

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import React from 'react';
 import Banner from './components/Banner';
 import Testimonials from './components/Testimonials';
 import Skills from './components/Skills';
+import Team from './components/Team';
 
 const App = () => {
   return <div>
     <Banner />
     <Testimonials />
     <Skills/>
+    <Team />
     <div className='h-[2000px]'></div>
   </div>;
 };
