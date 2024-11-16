f8bda5599cda7418aeb4b4ebbcc5e2dc9512752f
Steven David Pillay <98861688+code-Gambler@users.noreply.github.com>
2024-11-16 16:40:34 -0500

    Finished Testimonials with autoplay

--- src/components/TestimonialSlider.js
+++ src/components/TestimonialSlider.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { testimonials } from '../data';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import '../testimonialSlider.css';
+
+// import Swiper core and required modules
+import { Autoplay, Pagination } from 'swiper';
 
 const TestimonialSlider = () => {
   return (
-    <div>TestimonialSlider</div>
-  )
+    <>
+    <Swiper pagination={{dynamicBullets: true, clickable: true }} autoplay={{delay: 4000, disableOnInteraction:false}} modules={[Autoplay, Pagination]} className='mySwiper'>
+      {testimonials.map((item, index) => {
+        const { text, name, image, position } = item;
+        return (
+        <SwiperSlide key={index}>
+          <div className='relative lg:mt-[34px] before:content-quote p-8'>
+            <div>
+              <p>{text}</p>
+              <div className='flex items-center mt-[18px] mb9'>
+                <img src={image} alt="" />
+                <div className='ml-4'>
+                  <h6 className='font-bold'>{name}</h6>
+                  <p className='text-sm'>{position}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>);
+      }
+      )}
+    </Swiper>
+    </>
+  );
 }
 
 export default TestimonialSlider
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import TestimonialSlider from './TestimonialSlider'
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section className='h-[346px] lg:mb-[120px]'>
+        <div className="container mx-auto h-full flex items-center justify-end">
+            <div className='w-full mx-auto lg:mx-0 lg:max-w-[570px]'>
+                <TestimonialSlider />
+            </div>
+        </div>
+    </section>
   )
 }
 
