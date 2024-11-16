f1bdbb80e82eba92d77e1bd896e74ec1a6251199
Steven David Pillay <98861688+code-Gambler@users.noreply.github.com>
2024-11-16 16:42:17 -0500

    Added boilerplate code for skills section

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 // import components
 import Banner from './components/Banner';
 import Testimonials from './components/Testimonials';
+import Skills from './components/Skills';
 
 const App = () => {
   return <div>
     <Banner />
     <Testimonials />
+    <Skills/>
     <div className='h-[2000px]'></div>
   </div>;
 };
