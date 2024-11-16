50c61901ad632bf8ff4287b86194820a04c6f36a
Steven David Pillay <98861688+code-Gambler@users.noreply.github.com>
2024-11-16 17:35:10 -0500

    Completed the Newsletter section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Banner from './components/Banner';
 import Testimonials from './components/Testimonials';
 import Skills from './components/Skills';
 import Team from './components/Team';
+import Newsletter from './components/Newsletter';
 
 const App = () => {
   return <div>
@@ -12,6 +13,7 @@ const App = () => {
     <Testimonials />
     <Skills/>
     <Team />
+    <Newsletter />
     <div className='h-[2000px]'></div>
   </div>;
 };
